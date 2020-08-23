c2b0bad9ff42f02372c14e1f36b9fbcea45ef509
aaSuarez <aasuarez17pro@gmail.com>
2020-08-23 18:53:20 -0300

    creamos nuestro sitio

---
+++ javascript/js.js
@@ -0,0 +1,14 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".titulo",{
+    duration:2000,
+    origin:'right'
+})
+sr.reveal(".derecha",{
+    duration:2000,
+    origin:'left'
+})
+sr.reveal(".info",{
+    duration:2000,
+    origin:'bottom'
+})
